60700e09baa96cf9f2ce465761cbf4968a4e94a0
leicongwu <917592309@qq.com>
2018-09-30 14:56:40 +0800

    学习

---
+++ spring_learn/src/test/java/com/didispace/web/HelloApplicationTest2.java
@@ -0,0 +1,23 @@
+package com.didispace.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by lifestorm on 2018/9/30.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloApplicationTest2 {
+
+    @Autowired
+    private BlogProperties blogProperties;
+
+    @Test
+    public void test1() throws Exception {
+        System.out.println(blogProperties.getName());
+    }
+}
